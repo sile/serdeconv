d65c082fd008efde320b72983622d09674886010
Takeru Ohta <phjgt308@gmail.com>
2017-05-06 17:09:10 +0900

    Add a crate level example.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,36 @@
 //! This crate provides convenient traits for converting between TOML and serializable values.
+//!
+//! # Examples
+//!
+//! Converts from a TOML string to a serializable value:
+//!
+//! ```
+//! extern crate serde;
+//! #[macro_use]
+//! extern crate serde_derive;
+//! extern crate tomlconv;
+//!
+//! use tomlconv::FromToml;
+//!
+//! // Defines a deserializable struct.
+//! #[derive(Deserialize)]
+//! struct Foo {
+//!     bar: String,
+//!     baz: usize
+//! }
+//! impl FromToml for Foo {}
+//!
+//! # fn main() {
+//! // Converts from the TOML string to a `Foo` value.
+//! let toml = r#"
+//! bar = "aaa"
+//! baz = 123
+//! "#;
+//! let foo = Foo::from_toml_str(toml).unwrap();
+//! assert_eq!(foo.bar, "aaa");
+//! assert_eq!(foo.baz, 123);
+//! # }
+//! ```
 #![warn(missing_docs)]
 extern crate serde;
 extern crate toml;
